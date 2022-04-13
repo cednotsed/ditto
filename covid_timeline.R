ecaf9cd019de27f3d6041d602092865d258822ce
cednotsed <cedriccstan@gmail.com>
2022-04-13 08:33:26 +0800

    added omicron

--- covid_timeline.R
+++ covid_timeline.R
@@ -21,12 +21,14 @@ country_stats <- df %>%
 
 plot_df <- country_stats %>%
   bind_rows(tibble(country = c("Pandemic declared", "Emergence in humans", "Audacity release in present study",
-                               "Earliest Alpha", "Earliest Beta", "Earliest Gamma", "Earliest Delta"), 
+                               "Earliest Alpha", "Earliest Beta", "Earliest Gamma", "Earliest Delta", "Earliest Omicron"), 
                    host = "Pandemic timeline",
                    start = as.Date(c("2020-03-11", "2019-10-06", "2022-03-17",
-                                     "2020-09-15", "2020-05-15", "2020-11-15", "2020-10-15")),
+                                     "2020-09-15", "2020-05-15", "2020-11-15", 
+                                     "2020-10-15", "2021-11-11")),
                    end = as.Date(c("2020-03-11", "2019-12-11", "2022-03-17",
-                                   "2020-09-15", "2020-05-15", "2020-11-15", "2020-10-15"))))
+                                   "2020-09-15", "2020-05-15", "2020-11-15", 
+                                   "2020-10-15", "2021-11-11"))))
 
 # Annotate mutation times
 time_meta <- fread("results/temporal_distribution/mutation_times.csv") %>%
@@ -44,14 +46,14 @@ time_plt <- gg_vistime(plot_df,
 
 for (i in seq(nrow(time_meta))) {
   time_plt <- time_plt +
-    geom_point(x = as.POSIXct(time_meta[i, ]$earliest), y = 4, color = "black") +
-    geom_text(x = as.POSIXct(time_meta[i, ]$earliest),
-              y = 4,
-              label = time_meta[i, ]$mut,
-              angle = 45,
-              size = 1,
-              color = "black")
+    geom_point(x = as.POSIXct(time_meta[i, ]$earliest), y = 4, color = "black")
+    # geom_text(x = as.POSIXct(time_meta[i, ]$earliest),
+    #           y = 4,
+    #           label = time_meta[i, ]$mut,
+    #           angle = 45,
+    #           size = 1,
+    #           color = "black")
 }
 
 time_plt
-ggsave("results/temporal_distribution/timeline.png", dpi = 300, width = 11, height = 5)
+ggsave("results/temporal_distribution/timeline.nolab.png", dpi = 300, width = 11, height = 5)
